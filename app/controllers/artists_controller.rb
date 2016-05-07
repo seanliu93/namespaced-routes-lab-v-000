9b9a0a01c4f2eb46c6f89220d0e4277ee049d913
seanliu93 <sean93liu@gmail.com>
2016-05-07 00:16:27 -0400

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -8,7 +8,12 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    pref = Preference.first
+     if pref && !pref.allow_create_artists
+       redirect_to artists_path
+     else
+       @artist = Artist.new
+     end
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'preferences/index'
+
   resources :artists do
     resources :songs, only: [:index, :show]
   end
   resources :songs
 
+  namespace :admin do
+    resources :preferences, only: [:index]
+  end
 end
